6459d5ffb2dad6b31d2189ccd719707fa77c7c11
diegozamora <diarzaza@gmail.com>
2026-07-02 12:57:08 -0500

    remove telefono_alternativo from cotizar form

--- leads/forms.py
+++ leads/forms.py
@@ -4,7 +4,7 @@ from .models import Lead
 class LeadForm(forms.ModelForm):
     class Meta:
         model = Lead
-        fields = ['nombre', 'telefono', 'email', 'direccion', 'telefono_alternativo', 'mensaje', 'imagen']
+        fields = ['nombre', 'telefono', 'email', 'direccion', 'mensaje', 'imagen']
         widgets = {
             'direccion': forms.Textarea(attrs={'rows': 3}),
             'mensaje': forms.Textarea(attrs={'rows': 5, 'placeholder': 'Describe el daño en el techo...'}),
